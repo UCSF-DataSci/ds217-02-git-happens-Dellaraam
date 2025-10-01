47e3ab7b9068916d513fe98a868f07738c4c5ad4
Dellaraam <136400422+Dellaraam@users.noreply.github.com>
2025-10-01 15:44:01 -0700

    src

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,37 +1,74 @@
-def load_students(filename):
+import sys
+
+def load_students(file):
     """Load student data from CSV."""
     # TODO: Open file, read lines, skip header
-    # TODO: Split each line by comma
-    # TODO: Return list of student data
+    with open(file) as file:
+        lines = file.readlines()
+        # TODO: Split each line by comma
+        students = []
+        for line in lines[1:]:
+            name, age, grade, subject = line.strip().split(",") 
+            students.append(
+                {
+                    'name': name,
+                    'age': int(age),
+                    'grade': int(grade),
+                    'subject': subject
+                }
+            )
+            # TODO: Return list of student data
+        return students
     pass
 
 def calculate_average_grade(students):
     """Calculate average grade."""
+    total = 0
+    for student in students:
+        total += student['grade']
     # TODO: Sum all grades
+    average = total/len(students)
     # TODO: Divide by number of students
+    return average
     pass
 
 def count_math_students(students):
     """Count students in Math."""
     # TODO: Count students where subject is Math
+    math_count = 0
+    for student in students:
+        if student['subject'] == "math":
+            math_count += 1
+    return math_count
     pass
 
 def generate_report(total, average, math_count):
     """Generate report string."""
     # TODO: Create formatted string with results
     # TODO: Use f-strings with .1f for decimals
+    report = f"Average grade: {average:.1f} Math Student Count: {math_count} Student Count: {total}"
+    return report 
     pass
 
-def save_report(report, filename):
+def save_report(report, file):
     """Save report to file."""
     # TODO: Create output directory if needed
     # TODO: Write report to file
+    with open('output/analysis_results.txt', 'w') as file:
+        file.write(f"{report}\n")
     pass
 
 def main():
+    file = sys.argv[1]
     # TODO: Load data
+    student_info = load_students(file)
     # TODO: Calculate statistics
+    ave = calculate_average_grade(student_info)
+    print(f'{ave}')
+    math = count_math_students(student_info)
+    print(f'{math}')
     # TODO: Generate and save report
+    save_report(generate_report(len(student_info),ave,math), student_info)
     pass
 
 if __name__ == "__main__":
--- src/data_analysis_functions.py
+++ src/data_analysis_functions.py
@@ -1,11 +1,31 @@
-def load_data(filename):
+import sys
+
+def load_data(file):
     """Load data from CSV file."""
     # TODO: Check file extension
+    if file.endswith(".csv"):
+        print("Correct File")
     # TODO: Call appropriate loader
     pass
 
-def load_csv(filename):
+def load_csv(file):
     """Load CSV data."""
+    with open(file) as file:
+        lines = file.readlines()
+        # TODO: Split each line by comma
+        students = []
+        for line in lines[1:]:
+            name, age, grade, subject = line.strip().split(",") 
+            students.append(
+                {
+                    'name': name,
+                    'age': int(age),
+                    'grade': int(grade),
+                    'subject': subject
+                }
+            )
+            # TODO: Return list of student data
+        return students
     # TODO: Same technique as basic script
     pass
 
@@ -13,20 +33,72 @@ def analyze_data(students):
     """Analyze student data."""
     # TODO: Calculate multiple statistics
     # TODO: Return dictionary of results
+    max_grade = 0
+    min_grade = 100
+    subjects = {"English": 0, "PE": 0, "History": 0, "Chemistry": 0, "Art": 0, "Biology": 0 }
+    for student in students:
+        if student['grade'] >= max_grade:
+            max_grade = student['grade']
+        if student['grade'] <= min_grade:
+            min_grade = student['grade']
+            
+        if student['subject'] == "English":
+            subjects["English"] += 1
+        if student['subject'] == "PE":
+            subjects["PE"] += 1
+        if student['subject'] == "History":
+            subjects["History"] += 1
+        if student['subject'] == "Chemistry":
+            subjects["Chemistry"] += 1
+        if student['subject'] == "Art":
+            subjects["Art"] += 1
+        if student['subject'] == "Biology":
+            subjects["Biology"] += 1
+    return max_grade, min_grade, subjects 
     pass
 
-def analyze_grade_distribution(grades):
+def analyze_grade_distribution(students):
     """Count grades by letter grade."""
     # TODO: Count A (90-100), B (80-89), etc.
+    letter_grades = {
+        'A (90-100)': 0,
+        'B (80-89)': 0,
+        'C (70-79)': 0,
+        'D (60-69)': 0,
+        'F (0-59)': 0
+        }
+    for student in students:
+        if student['grade'] >= 90:
+            letter_grades['A (90-100)'] += 1
+        if student['grade'] >= 80:
+            letter_grades['B (80-89)'] += 1
+        if student['grade'] >= 70:
+            letter_grades['C (70-79)'] += 1
+        if student['grade'] >= 60:
+            letter_grades['D (60-69)'] += 1
+        else:
+            letter_grades['F (0-59)'] += 1
+    return letter_grades
     pass
 
-def save_results(results, filename):
+def save_results(letter_grades, max_grade, min_grade, subjects):
     """Save detailed results."""
     # TODO: Format and write comprehensive report
+    with open('output/analysis_results.txt', 'a') as file:
+        file.write(f'Grade Distrobustion: {letter_grades}\n')
+        file.write(f'Maximum Grade: {max_grade}\n')
+        file.write(f'Minimum Grade: {min_grade}\n')
+        file.write(f'Student Subjects: {subjects}\n')
     pass
 
 def main():
     # TODO: Orchestrate the analysis
+    file = load_csv(sys.argv[1])
+
+    print(analyze_data(file))
+    max_grade, min_grade, subjects = analyze_data(file)
+    print(analyze_grade_distribution(file))
+    save_results(analyze_grade_distribution(file), max_grade, min_grade, subjects)
     pass
 
 if __name__ == "__main__":
